536a871004311bba5b9d6d30a1ffceb3b3ba3839
Joel Hans <joel@joelhans.com>
2012-04-27 16:23:45 -0500

    Added feeds.js, which will soon hold most logic relating to feeds.

---
+++ feeds.js
@@ -0,0 +1,94 @@
+module.exports = function(app, express, loadUser, Users, Feeds, req){
+
+//  feeds.js
+// 
+//  This file handles all the functions relating to RSS feeds.
+//
+//  Order of functions:
+//    * Function to get, return all a user's feeds/subscriptions.
+//    * Editing feeds
+
+  function getAllFeeds() {
+    Users.findOne({ $or : [ { 'username': req.session.userID }, { 'email': req.session.userID } ] }, function(err, result) {
+      getFeeds(result['email'], result['username'], function(error, feeds, podcastList) {
+        res.partial('partials/podcasts', { feeds: feeds, podcasts: podcastList });
+      });
+    });
+  }
+
+  //
+  //  EDIT A FEED
+  //
+
+  function editFeed() {
+    console.log('Let\'s go!');
+  }
+
+  getFeeds = function(email, username, callback) {
+
+    var construction = new Array(),
+      podList = new Array(),
+      unlistened = new Array(),
+      i,
+      j;
+
+    Feeds.find( { $or : [ { 'owner': email }, { 'owner': username } ] } ).toArray(function(err, results) {
+
+      // console.log(results);
+
+      if (typeof results[0] == "undefined") {
+        callback(null, [], []);
+      } else {
+        var i,
+          j,
+          podcastList = new Array(),
+          construction = new Array();
+
+        for (i = 0; i < results.length; ++i) {
+          data = {
+            feedTitle     : results[i].title,
+            feedDescription : results[i].description,
+            feeduuid    : results[i].uuid
+          }
+          construction.push(data);
+        }
+
+        construction.sort(function (a , b) {
+            if (a['feedTitle'].toLowerCase() > b['feedTitle'].toLowerCase())
+              return 1;
+            if (a['feedTitle'].toLowerCase() < b['feedTitle'].toLowerCase())
+              return -1;
+            return 0;
+          });
+
+        for (i = 0; i < results.length; ++i) {
+          for (j = 0; j < results[i].pods.length; ++j) {
+            var podData = results[i].pods[j];
+
+            podData['feedTitle'] = results[i].title;
+            podData['feedUUID'] = results[i].uuid;
+
+            if ( results[i].pods[j].listened != "true" ) {
+              podcastList.push(podData);
+            }
+          }
+        }
+
+        if (typeof podcastList[0] === "undefined") {
+          return;
+        } else {
+          podcastList.sort(function (a , b) {
+            if (a['podDate']['_d'] > b['podDate']['_d'])
+              return -1;
+            if (a['podDate']['_d'] < b['podDate']['_d'])
+              return 1;
+            return 0;
+          });
+        }
+
+        callback(null, construction, podcastList);
+      }
+    });
+  }
+
+};
